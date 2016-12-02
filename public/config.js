6ad52830970747911e85438fdd8793edbbb4b342
Andy Wolff <andy.wolff@gmail.com>
2016-12-02 15:45:02 -0600

    "this"

---
+++ public/config.js
@@ -0,0 +1,41 @@
+/*
+
+########  ######## ########  ##          ###     ######  ########
+##     ## ##       ##     ## ##         ## ##   ##    ## ##
+##     ## ##       ##     ## ##        ##   ##  ##       ##
+########  ######   ########  ##       ##     ## ##       ######
+##   ##   ##       ##        ##       ######### ##       ##
+##    ##  ##       ##        ##       ##     ## ##    ## ##
+##     ## ######## ##        ######## ##     ##  ######  ########
+
+######## ##     ## ####  ######
+   ##    ##     ##  ##  ##    ##
+   ##    ##     ##  ##  ##
+   ##    #########  ##   ######
+   ##    ##     ##  ##        ##
+   ##    ##     ##  ##  ##    ##
+   ##    ##     ## ####  ######
+
+   ######## #### ##       ########
+   ##        ##  ##       ##
+   ##        ##  ##       ##
+   ######    ##  ##       ######
+   ##        ##  ##       ##
+   ##        ##  ##       ##
+   ##       #### ######## ########
+
+*/
+
+// You will need to update these with your own values
+// Do not copy the script tags into here, just the config part
+
+/////////////////////////////////////////////////////////
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBjiF2rAV4NpBp9aTuNo4i_kjzVn99DZuE",
+  authDomain: "firenode-77722.firebaseapp.com",
+  databaseURL: "https://firenode-77722.firebaseio.com",
+  storageBucket: "firenode-77722.appspot.com",
+  messagingSenderId: "957713091670"
+};
+firebase.initializeApp(config);
